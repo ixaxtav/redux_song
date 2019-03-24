13023908543a9617385ee16f0a8de19fdd5272c7
Ixax Tavarez <i.tavarez@umiami.edu>
2019-03-24 00:48:30 +0000

    created songlist exported with connect

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -12,7 +12,7 @@ const songReducer = () => {
 };
 
 const selectedSongReducer = (selectedSong=null, action) => {
-    if (action.type == 'SONG_SELECTED') {
+    if (action.type === 'SONG_SELECTED') {
         return action.payload;
     }
     return selectedSong;
