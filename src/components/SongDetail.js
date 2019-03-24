4eb1b5f975459217bfaa6e213283ccac189d32a2
Ixax Tavarez <i.tavarez@umiami.edu>
2019-03-24 16:25:11 +0000

    song detail added

---
+++ src/components/SongDetail.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+const SongDetail = () => {
+  return <div> Song Detail</div>;  
+};
+
+const mapStateToProps = state => {
+   return {song: state.selectedSong};
+};
+
+export default connect(mapStateToProps)(SongDetail);
