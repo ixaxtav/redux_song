13875cd1cb074f510a0e1ff971c5a91623198041
Ixax Tavarez <i.tavarez@umiami.edu>
2019-03-24 16:30:02 +0000

    app completed simple song select application using react redux

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,8 +1,24 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
-const SongDetail = () => {
-  return <div> Song Detail</div>;  
+const SongDetail = ({song}) => {
+    if(!song){
+        return <div>Select a song</div>;
+    }
+    
+  return (
+        <div>
+            <h3> Details for: </h3>
+            <p>
+                Title: {song.title}
+                <br/>
+                Duration: {song.duration}
+            
+            </p>
+            
+        </div>
+      
+      )
 };
 
 const mapStateToProps = state => {
