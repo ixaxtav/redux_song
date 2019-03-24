47d8a8610c78d20e298de0350a4034b92769b9e6
Ixax Tavarez <i.tavarez@umiami.edu>
2019-03-24 00:31:24 +0000

    created two reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,17 @@
+const songReducer = () => {
+  return [
+      {title: 'Mamba', duration: "2:02"},
+      {title: 'Con te partiro', duration: "2:45"},
+      {title: 'Reptilia', duration: "3:12"},
+      {title: 'Ave Maria', duration: "2:52"}
+      
+      
+      ]  ;
+};
+
+const selectedSongReducer = (selectedSong=null, action) => {
+    if (action.type == 'SONG_SELECTED') {
+        return action.payload;
+    }
+    return selectedSong;
+};
